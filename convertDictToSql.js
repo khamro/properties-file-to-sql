c4ce89f4f65b19ed2976a254038175c08d990d6d
khamro <amro.khatib@amdocs.com>
2017-08-30 13:08:03 +0300

    Add files via upload

--- convertDictToSql.js
+++ convertDictToSql.js
@@ -13,9 +13,6 @@ function createFile(fileName) {
                 console.log("The file was saved!");
             });
         } else {
-            // fs.writeFileSync(FILE_NAME, '', 'utf8', function () {
-            //     console.log("The file exists!");
-            // });
             console.log("The file exists!");
             
         }
@@ -39,8 +36,8 @@ function convertToSql() {
     var properties = PropertiesReader('./'+fileName);
     var fileNameArray = fileName.split('_');
     var namespace = fileNameArray[0];
-    var local = fileNameArray[1].split('.')[0];
-    console.log(namespace +'  ' + local);
+    var locale = fileNameArray[1].split('.')[0];
+    console.log(namespace +'  ' + locale);
     properties.each(function (key, value) {
         if(key.length > 36){
             console.error("key lenght is "+ key.length + " Key length must be 36 characters or less,the key is: " + key)
@@ -51,15 +48,16 @@ function convertToSql() {
         (
             '${key}',
             '${value}',
-            'This is a key value message for localization dictionary',
+            'localization message',
             '${namespace}',
             0,
-            '${local}'
+            '${locale}'
         );\n`
         if (fs.existsSync(targetFileName)) {
             // console.log(sqlTmp);
             fs.appendFileSync(targetFileName, sqlTmp);
         }
     });
+    fs.appendFileSync(targetFileName, "commit;");
 }
 convertToSql();
